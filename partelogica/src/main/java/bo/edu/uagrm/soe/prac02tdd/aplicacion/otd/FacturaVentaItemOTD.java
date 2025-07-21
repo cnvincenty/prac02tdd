b8b34435fceeac3064d30850d220680fb53f6849
Juan Jose Miranda <jjsmm97@gmail.com>
2025-07-21 00:10:01 -0400

    feat: create FacturaVentaTest and entity

---
+++ partelogica/src/main/java/bo/edu/uagrm/soe/prac02tdd/aplicacion/otd/FacturaVentaItemOTD.java
@@ -0,0 +1,17 @@
+package bo.edu.uagrm.soe.prac02tdd.aplicacion.otd;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class FacturaVentaItemOTD {
+    private Long productoId;
+    private String productoNombre;
+    private String productoGrupo;
+    private BigDecimal cantidad;
+    private BigDecimal precioUnitario;
+    private BigDecimal porcentajeDescuento;
+    private BigDecimal montoDescuento;
+    private BigDecimal subtotal;
+}
